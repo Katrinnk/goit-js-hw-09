2987f350ad883b44abc0b9e5295a1ae990e7049c
Katrinnk <katerin69llobster@gmail.com>
2023-08-01 21:30:27 +0300

    hwp1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+function startSwitchColor(){
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    timerId = setInterval(() => {
+    let randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+}
+
+function stopSwitchColor() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+
+    clearInterval(timerId);
+}
+
+btnStart.addEventListener('click', startSwitchColor);
+btnStop.addEventListener('click', stopSwitchColor);
