ef41a46f98f2fdf4c3196f2e10a8565fccda704c
Katrinnk <72568692+Katrinnk@users.noreply.github.com>
2023-08-01 19:42:19 +0000

    Deploying to gh-pages from @ Katrinnk/goit-js-hw-09@d9110beb4dc26b5336c5dcff2e2efc551f64a581 🚀

--- 01-color-switcher.f9465054.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,timerId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.f9465054.js.map
